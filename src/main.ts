1a7e4b151371dd702ae0742de8dedd79ca573aaa
EazyPau <poyizhi@gmail.com>
2022-01-22 09:57:38 +0800

    clear repo

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import router from "/@/router/router.js";
-import App from './App.vue'
-import './index.css'
-
-const app = createApp(App)
-
-app.use(router)
-app.mount("#app")
